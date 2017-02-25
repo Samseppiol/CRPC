b2f49e76b5c95e3bbd98f24d063a64eee39a0d9c
Samseppiol <j_hage@live.com>
2017-02-25 13:39:27 +1100

    While loops

---
+++ ruby/while_loops.rb
@@ -0,0 +1,8 @@
+i = 0
+
+#While loops always have to have an increment!! Otherwise it will infinite loop.
+#i.e If you do not change the value of i, i will always be less than 10 and will be infinite.
+while i < 10
+  puts "Hey there"
+  i += 1
+end
