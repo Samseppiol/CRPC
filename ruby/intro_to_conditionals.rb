4441716fcf3c6c6dfcfd6146ac9dea6bc2ede77d
Samseppiol <j_hage@live.com>
2017-02-26 20:20:57 +1100

    Very basic introduction to if statements in ruby

---
+++ ruby/intro_to_conditionals.rb
@@ -0,0 +1,17 @@
+#Defining two variables.
+x = 10
+y = 5
+#An if statement.
+if x == y
+  puts 'x is the same as y'
+else
+  puts 'x and y are not the same'
+end
+
+j = 20
+k = 20
+if j == k
+  puts 'j is the same as k'
+else
+  puts 'j is not the same as k'
+end
