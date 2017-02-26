73693cf64d50407a49353aab3166bd5a9b502ab5
Samseppiol <j_hage@live.com>
2017-02-26 17:58:01 +1100

    Using the .join method on arrays, once again Jordan demonstrates this in IRB but I saved a few examples for later use

---
+++ ruby/join_arrays.rb
@@ -0,0 +1,7 @@
+#Declaring our array, I used la liga football teams.
+teams = ['Real Madrid', 'Barcelona', 'Valencia', 'Athletico Madrid']
+p teams
+#using .join, and whatever is in the parentheses will join the items of the array together.
+puts teams.join("____")
+puts teams.join("****")
+puts teams.join("-_-_-_-")
