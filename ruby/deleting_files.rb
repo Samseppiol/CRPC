5cae795713d518f883370924e89daf1bfa3843a2
Samseppiol <j_hage@live.com>
2017-02-27 20:12:49 +1100

    How to use ruby to delete files, scary stuff, no warnings given just wam bam gtfo

---
+++ ruby/deleting_files.rb
@@ -0,0 +1,3 @@
+#How to delete files in ruby. No warning messages are given, easy to delete the wrong thing. And control Z does not work.
+#What happened to the emails hilary?
+File.delete("hilarys_emails.txt")
