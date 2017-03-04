325c18ed77f0bb3e4c3bc1c13ea3da29eadf255e
Samseppiol <j_hage@live.com>
2017-03-04 17:37:50 +1100

    Basic introduction to algorithms, Jordan shows a few examples on a webpage of different sorting algorithms and we used an inbuilt sort algortihm over an array in ruby.

---
+++ ruby/algorithms_overview.rb
@@ -0,0 +1,7 @@
+#Create an array
+a = [1, 4, 6, 9 ,2 ,3 ,22, 44, 66, 1]
+#Sort out array. Wouldn't really need our own sorting algorithm when ruby can already do it.
+#.sort is actually a 'quick sort' algorithm built in to ruby. Amazing we can do such a complex
+#task with one line of code!
+p a.sort
+#Because learning how to write a sorting algorithm is base knowledge for computer science.
